d3dcced61a19f575417badb23f09ee797297e668
ahmed-kaif <kaifkhan.rcs@gmail.com>
2021-04-08 22:02:56 +0600

    added drawing methods

--- simple-drawing-app/src/components/HelloWorld.vue
+++ simple-drawing-app/src/components/HelloWorld.vue
@@ -1,106 +1,70 @@
 <template>
   <div class="hello">
+    <span>{{ x }},{{ y }}</span>
     <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br />
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener"
-        >vue-cli documentation</a
-      >.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel"
-          target="_blank"
-          rel="noopener"
-          >babel</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-router"
-          target="_blank"
-          rel="noopener"
-          >router</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint"
-          target="_blank"
-          rel="noopener"
-          >eslint</a
-        >
-      </li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li>
-        <a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a>
-      </li>
-      <li>
-        <a href="https://forum.vuejs.org" target="_blank" rel="noopener"
-          >Forum</a
-        >
-      </li>
-      <li>
-        <a href="https://chat.vuejs.org" target="_blank" rel="noopener"
-          >Community Chat</a
-        >
-      </li>
-      <li>
-        <a href="https://twitter.com/vuejs" target="_blank" rel="noopener"
-          >Twitter</a
-        >
-      </li>
-      <li>
-        <a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a>
-      </li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li>
-        <a href="https://router.vuejs.org" target="_blank" rel="noopener"
-          >vue-router</a
-        >
-      </li>
-      <li>
-        <a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-devtools#vue-devtools"
-          target="_blank"
-          rel="noopener"
-          >vue-devtools</a
-        >
-      </li>
-      <li>
-        <a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener"
-          >vue-loader</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-          rel="noopener"
-          >awesome-vue</a
-        >
-      </li>
-    </ul>
+    <canvas
+      id="myCanvas"
+      height="500"
+      width="800"
+      @mousemove="draw"
+      @mousedown="beginDrawing"
+      @mouseup="stopDrwaing"
+    ></canvas>
+    <button @click="clear">Clean</button>
   </div>
 </template>
 
 <script>
 export default {
-  name: "HelloWorld",
+  name: 'Draw',
   props: {
     msg: String,
   },
-};
+  data() {
+    return {
+      canvas: null,
+      x: 0,
+      y: 0,
+      isDrwaing: false,
+    }
+  },
+  methods: {
+    clear() {
+      this.canvas.clearRect(0, 0, 800, 500)
+    },
+    drawLine(x1, y1, x2, y2) {
+      let ctx = this.canvas
+      ctx.beginPath()
+      ctx.strokeStyle = 'black'
+      ctx.lineWidth = 1
+      ctx.moveTo(x1, y1)
+      ctx.lineTo(x2, y2)
+      ctx.stroke()
+      ctx.closePath()
+    },
+    beginDrawing(e) {
+      this.x = e.offsetX
+      this.y = e.offsetY
+      this.isDrwaing = true
+    },
+    stopDrwaing() {
+      this.x = 0
+      this.y = 0
+      this.isDrwaing = false
+    },
+    draw(e) {
+      if (this.isDrwaing) {
+        this.drawLine(this.x, this.y, e.offsetX, e.offsetY)
+        this.x = e.offsetX
+        this.y = e.offsetY
+      }
+    },
+  },
+  mounted() {
+    let c = document.getElementById('myCanvas')
+    this.canvas = c.getContext('2d')
+  },
+}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
@@ -118,5 +82,8 @@ li {
 }
 a {
   color: #42b983;
+}
+#myCanvas {
+  border: 1px solid black;
 }
 </style>
